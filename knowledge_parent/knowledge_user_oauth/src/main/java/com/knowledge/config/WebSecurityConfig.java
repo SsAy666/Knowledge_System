0b0eadf19f3ed3316fcddf853d50f2032d731b96
linzishan <linzishan@rtmap.com>
2021-03-29 19:02:07 +0800

    跨域问题待解决

--- knowledge_parent/knowledge_user_oauth/src/main/java/com/knowledge/config/WebSecurityConfig.java
+++ knowledge_parent/knowledge_user_oauth/src/main/java/com/knowledge/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import com.knowledge.service.impl.auth.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +12,8 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsUtils;
 
 /**
  * Spring Security的配置类
@@ -25,23 +28,38 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                // 如果有允许匿名的url，填在下面
-//                .antMatchers().permitAll()
-//                .anyRequest().authenticated()
+        //开启跨域 cors()
+        http.cors().and().csrf().disable().authorizeRequests()
+                //处理跨域请求中的Preflight请求
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                .antMatchers(HttpMethod.POST,"/").permitAll()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
                 .and()
-                // 设置登陆页
-                .formLogin().loginPage("http://localhost:9528/#/login")
-                // 设置登陆成功页
-                .defaultSuccessUrl("/").permitAll()
-                // 自定义登陆用户名和密码参数，默认为username和password
-//                .usernameParameter("username")
-//                .passwordParameter("password")
+                .formLogin()
+                //自定义登录页面
+                .loginPage("/login")
                 .and()
-                .logout().permitAll();
+                .logout();
+        http.formLogin().loginProcessingUrl("user/login").permitAll();
 
+
+        //        什么是（cors 预检请求） 就是你要跨域请求得时候 你要预先发一个请求看对面是拦你还是放你
+        //第1步：解决跨域问题。cors 预检请求放行,让Spring security 放行所有preflight request（cors 预检请求）
+       /* http.formLogin().loginProcessingUrl("user/login").permitAll();
+        http.authorizeRequests().requestMatchers(CorsUtils::isPreFlightRequest).permitAll();
+
+        //配置权限
+        http.authorizeRequests()
+//                 放行注册请求
+                .antMatchers(HttpMethod.POST,"/user/register").permitAll()
+                .anyRequest()
+                .access(("@dynamicPermission.checkPermisstion(authentication)"));
+                // 设置登陆成功页
+//                .defaultSuccessUrl("/")
+//                .permitAll();
         // 关闭CSRF跨域
-        http.csrf().disable();
+        http.csrf().disable();*/
     }
 
     @Override
