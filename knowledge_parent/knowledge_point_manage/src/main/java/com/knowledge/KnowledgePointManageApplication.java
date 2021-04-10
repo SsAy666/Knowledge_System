42d197624e548ee2c2c9df7a4111e506208c9b3b
linzishan <linzishan@rtmap.com>
2021-04-10 10:03:01 +0800

    知识点内容增删改查

--- knowledge_parent/knowledge_point_manage/src/main/java/com/knowledge/KnowledgePointManageApplication.java
+++ knowledge_parent/knowledge_point_manage/src/main/java/com/knowledge/KnowledgePointManageApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import javax.servlet.MultipartConfigElement;
 
